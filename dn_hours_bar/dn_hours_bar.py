6b24d4e27e9d6be93278fadfd1430afb4fb1b499
Derek Weitzel <djw8605@gmail.com>
2011-06-22 15:59:44 -0500

    Added wallduration filter.  Remove the jobs that have 0 runtime

--- dn_hours_bar/dn_hours_bar.py
+++ dn_hours_bar/dn_hours_bar.py
@@ -93,9 +93,10 @@ def QueryTG(req, options):
     LEFT JOIN acct.allocation_breakdown a ON ( j.allocation_breakdown_id = a.allocation_breakdown_id ) \
     LEFT JOIN acct.people p ON (a.person_id = p.person_id and (p.first_name || ' ' || p.last_name) ~* %(user)s) \
     LEFT JOIN acct.resources r on (r.resource_id = j.resource_id and r.resource_code ~* %(facility)s) \
-    where (end_time >= %(starttime)s \
+    where end_time >= %(starttime)s \
     AND end_time < %(endtime)s \
-    AND j.allocation_breakdown_id = a.allocation_breakdown_id) \
+    AND j.allocation_breakdown_id = a.allocation_breakdown_id \
+    AND wallduration > 0 \
     group by dn, EndTime", options))
 
 
@@ -107,9 +108,10 @@ def QueryTG(req, options):
     LEFT JOIN acct.allocation_breakdown a ON ( j.allocation_breakdown_id = a.allocation_breakdown_id ) \
     LEFT JOIN acct.people p ON (a.person_id = p.person_id and (p.first_name || ' ' || p.last_name) ~* %(user)s) \
     LEFT JOIN acct.resources r on (r.resource_id = j.resource_id and r.resource_code ~* %(facility)s) \
-    where (end_time >= %(starttime)s \
+    where end_time >= %(starttime)s \
     AND end_time < %(endtime)s \
-    AND j.allocation_breakdown_id = a.allocation_breakdown_id) \
+    AND j.allocation_breakdown_id = a.allocation_breakdown_id \
+    AND wallduration > 0 \
     group by dn, EndTime", options)
     
     data = cur.fetchone()
